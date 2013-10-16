b83b125b48fa4ed367a8344959d1ec4549b8dba8
Nicolas Tarzia <github@nicolastarzia.com>
2013-10-15 21:49:50 -0300

    Update run-install.ps1
    Changed to use some programs

--- run-install.ps1
+++ run-install.ps1
@@ -4,40 +4,78 @@ param([String]$scriptsFolder)
 $chocolateyInstallScript = Join-Path $scriptsFolder "install-chocolatey.cmd"
 & $chocolateyInstallScript
 
+
+# Framework
+cinstm ruby
+cinstm python
+cinstm nodejs.install
+
 # Visual Studio and extensions
 cinstm VisualStudio2012Ultimate
+cinstm visualstudio2013Ultimate
 
-cinstm resharper -Version 7.1.3000.2254
-cinstm resharper-nuget # makes resharper invoke NuGet when adding reference to an assembly in a NuGet package
+# Visual studio add-ins and stuff
+cinstm xUnit.vs2012
 cinstm WebEssentials2012
+cinstm resharper -Version 7.1.3000.2254 # Use only resharper if you want the latests. This is what I have a license for
+cinstm typescript
+cinstm typescript.vs
+cinstm SpecFlow
+cinstm resharper-nuget # makes resharper invoke NuGet when adding reference to an assembly in a NuGet package
 cinstm AnkhSvn # Subversion Source Control Provider
 
 # Browsers
 cinstm Firefox
-cinstm GoogleChrome
+cinstm GoogleChrome.Canary
+# Extensions that I liked
+cinst EthanBrown.ChromeCanaryDevExtensions
 
 # Tools
-cinstm fiddler
+cinstm nunit
+cinstm aspnetmvc
+cinstm fiddler4
 cinstm linqpad4
 cinstm expresso
 cinstm curl
 cinstm MetroStudio # Metro Studio - advanced icon editor
+cinstm sysinternals
+cinstm cyg-get # utility to install Cygwin packages and dependencies
+cinstm winrar
+cinstm winmerge
+cinstm windbg
+cinstm logparser
 
 # Utilities
+cinstm ccleaner
+cinstm paint.net
 cinstm 7zip
 cinstm tortoisesvn -Version 1.7.12 # I need this specific version.. don't ask why
+cinstm git
+cinstm gitextensions
+cinstm githubforwindows
+cinstm ilspy
+cinstm dotpeek
+cinstm putty
 
-cinstm procexp # Process Explorer
-cinstm procmon # Process Monitor
+cinstm virtualbox
+cinstm sourcecodepro
 
-cinstm console2
-cinstm cyg-get # utility to install Cygwin packages and dependencies
-
+cinstm conemu # ConEmu, it's better then Console2
+cinstm Far
+cinstm EthanBrown.ConEmuConfig # Some config to ConEmu
 cinstm filezilla
-cinstm dropbox
 
-cinstm notepadplusplus	# Notepad++
+#Cloud Storage
+cinstm dropbox
+cinstm googledrive
+cinstm skydrive
+
+cinstm sublimetext3    # SublimeText
+cinst SublimeText3.PackageControl -Pre
+cinstm TotalCommander
 cinstm adobereader		# Adobe Reader
 
 # IM
 cinstm skype
+
+

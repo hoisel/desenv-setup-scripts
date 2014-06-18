5ee70fa228deb30bd27a19e5ff3ac8bc6813f53d
Hoisel <danielhoiselferraz@gmail.com>
2014-06-17 21:24:05 -0300

    Alteração na lista de pacotes a serem instalados

--- run-install.ps1
+++ run-install.ps1
@@ -7,3 +7,34 @@ $chocolateyInstallScript = Join-Path $scriptsFolder "install-chocolatey.cmd"
 # Framework
 cinst nodejs.install -force
 
+# Browsers
+cinst Firefox -force
+cinst GoogleChrome.Canary -force
+
+# Utilitários
+cinst linqpad4 -force
+cinst NugetPackageExplorer -force
+cinst ccleaner -force
+cinst 7zip -force
+cinst git -force
+cinst gitextensions -force
+cinst githubforwindows
+cinst ilspy -force 
+cinst dotpeek -force
+#cinst sourcecodepro -force
+cinst filezilla -force
+cinst adobereader -force		
+cinst notepadplusplus -force
+cinst FoxitReader -force
+cinst TeraCopy -force
+cinst treesizefree -force
+cinst teamviewer -force
+cinst speccy -force
+cinst beyondcompare -force
+
+# IM
+cinst skype -force
+cinst pidgin -force
+
+#
+cinst k-litecodecpackfull -force
